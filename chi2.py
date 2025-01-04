a6eaff6e4bbb18906b11c9f295a82aa1d88894e4
ewearpel <ewearpel@gmail.com>
2025-01-04 11:25:18 +0100

    chi2 test implemented using .tsv files as input. Next: creating an appropriate output file and the nextflow pipeline

--- chi2.py
+++ chi2.py
@@ -2,13 +2,41 @@ import sys
 import json
 from scipy.stats import chi2_contingency
 import pandas as pd
+import re
 
-def filter_kmer_for_chi2()
 
+
+def chi2_test(tsv_paths):
+    # store input dataframes in a dictionary
+    chi2_dict = {}
+
+    for path in tsv_paths:
+        # retrieve keys (k-mer length) from file names
+        match = re.search(r'k=(\d+)', path)
+        if match:
+            key = f"k={match.group(1)}"
+
+        #set up dictionary to be used to store chi2 results
+        chi2_results = {}
+
+        #read tsv files, convert df to dict and store in df_dict with appropriate key
+        with open(path, 'r') as file:
+            df = pd.read_csv(file, sep='\t')
+            numeric_data = df.select_dtypes(include=["number"])
+            print(numeric_data)
+
+            chi2, p, dof, expected = chi2_contingency(numeric_data)
+            chi2_results['chi2'] = float(chi2)
+            chi2_results['p'] = float(p)
+            chi2_results['dof'] = dof
+            chi2_results['expected'] = expected
+
+            chi2_dict[key] = chi2_results
+
+    return chi2_dict
 
 if __name__ == '__main__':
-    kmer_dict_path = sys.argv[1]
+    tsv_paths = list(sys.argv[1].split(','))
 
-    with open(kmer_dict_path, 'r') as file:
-        content = file.read()
-        kmer_dict = json.loads(content)
+    chi2_dict = chi2_test(tsv_paths)
+    print(chi2_dict)
